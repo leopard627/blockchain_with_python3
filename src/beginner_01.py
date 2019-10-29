9ac582a11043e08c14a9bcc532c2242543d58a42
김지훈 <jh.k@coinone.com>
2019-10-30 05:51:42 +0900

    [master]

--- src/beginner_01.py
+++ src/beginner_01.py
@@ -1,9 +1,22 @@
 
-def sample_module():
-    """TODO: Docstring for module.
+BLOCK_CHAIN = []
 
-    :arg1: TODO
-    :returns: TODO
+def sample_module():
+    """TODO: 유닛 스모크 테스트용 True 리턴 전용함수
+
+    :returns: True
 
     """
     return True
+
+
+def get_status():
+    print(BLOCK_CHAIN)
+
+
+def get_last_blockchain_value():
+    return BLOCK_CHAIN[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    BLOCK_CHAIN.append([last_transaction, transaction_amount])
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.beginner_01 import sample_module
+from src.beginner_01 import sample_module, add_value, get_status
 
 class TestClass(unittest.TestCase):
     def setUp(self):
@@ -12,3 +12,8 @@ class TestClass(unittest.TestCase):
     def test_smoke_test(self):
         assert True == sample_module()
 
+    def test_block_chain_return_value_test(self):
+        add_value(10)
+        add_value(10, 20)
+        add_value(20, 30)
+        get_status()
